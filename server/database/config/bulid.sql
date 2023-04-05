1de7ddd5f6cba34b689d123abc587b5549a25cdf
Amal-Mousa <amalalsattari@gmail.com>
2023-04-05 11:12:47 +0300

    create-tables Relates #9;

--- server/database/config/bulid.sql
+++ server/database/config/bulid.sql
@@ -8,9 +8,25 @@ CREATE TABLE note (
     body text NOT NULL
 );
 
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE note_tags (
+    id SERIAL PRIMARY KEY,
+    note_id INTEGER REFERENCES note(id),
+    tag_id INTEGER REFERENCES tags(id)
+);
+
+
 INSERT INTO  note (title, body) VALUES 
     ('title', 'this is the body for the note'),
     ('title1', 'this is the body for the note1');
 
-
+INSERT INTO tags (name) VALUES
+    ('Work'),
+    ('sport'),
+    ('health');
+ 
 COMMIT;
