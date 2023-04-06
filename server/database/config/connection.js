75231b0b95f530eb1b7ef57e5f94ea73263cb8bb
Amal-Mousa <amalalsattari@gmail.com>
2023-04-06 14:10:29 +0300

    render Relates #17

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+let dbUrl = "";
+let ssl = false;
+
+const nodeEnv = process.env.NODE_ENV;
+
+if (nodeEnv === "production") {
+  dbUrl = process.env.DB_URL;
+  ssl = true;
+} else if (nodeEnv === "development") {
+  dbUrl = process.env.DEV_BD_URL;
+}
+
 const connection = new Pool({
-  connectionString: process.env.BD_URL,
-  ssl: false,
+  connectionString: dbUrl,
+  ssl,
 });
 
 module.exports = connection;
